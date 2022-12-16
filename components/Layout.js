59cc3d4749b92b94cf7ababfe7da205212d19c62
Deni Sugiarto <sugiarto.deni@gmail.com>
2022-12-16 11:10:24 +0700

    optimize SEO

--- components/Layout.js
+++ components/Layout.js
@@ -2,10 +2,31 @@ import Head from "next/head";
 import { UserProvider } from "../library/authContext";
 import Nav from "./Nav";
 
-const Layout = ({ user, loading = false, children }) => (
+const Layout = ({
+  user,
+  loading = false,
+  children,
+  title,
+  description,
+  imageUrl,
+}) => (
   <UserProvider value={{ user, loading }}>
     <Head>
-      <title>Film Database</title>
+      <meta name="robots" content="index, follow" />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={description} />
+      <meta name="og:image" content={imageUrl} />
+      <meta name="og:type" content="website" />
+      <meta name="twitter:card" content="player" />
+      <meta name="twitter:site" content="@filmdatabase" />
+      <meta name="twitter:url" content="myurl" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={imageUrl} />
+      <meta name="referrer" content="origin-when-cross-origin" />
     </Head>
     <Nav />
     <main className="px-4">
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,30 @@
-import Layout from '../components/Layout';
-import { useFetchUser } from '../library/authContext';
+import Layout from "../components/Layout";
+import { useFetchUser } from "../library/authContext";
 
 const Home = () => {
   const { user, loading } = useFetchUser();
   return (
-    <Layout user={user}>
+    <Layout
+      user={user}
+      title="Film Database"
+      description="Film database with nextJS"
+      imageUrl="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.co.id%2FAttraction_Review-g499447-d152724-Reviews-Zona_Arqueologica_de_Coba-Coba_Yucatan_Peninsula.html&psig=AOvVaw0Sv1DSOk7xW5zXz04ya3xF&ust=1671250117413000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLD7l4Oi_fsCFQAAAAAdAAAAABAD"
+    >
       <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter mb-4">
-        Strapi{' '}
+        Strapi{" "}
         <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
           Film Reviews
         </span>
       </h1>
       <p className="py-4">
-        This is a sample website recreated by{' '}
+        This is a sample website recreated by{" "}
         <a
           className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 hover:bg-gradient-to-r hover:from-teal-400 hover:to-blue-500"
           href="https://github.com/denisugiarto"
         >
           Deni Sugiarto
-        </a>{' '}
-        from{' '}
+        </a>{" "}
+        from{" "}
         <a
           className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 hover:bg-gradient-to-r hover:from-teal-400 hover:to-blue-500"
           href="https://jamstack.training"
